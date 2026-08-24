3a37ffc89aec9241261fc0b7fa6253e453914711
David OC <davidherzlos@gmail.com>
2026-08-23 21:46:16 -0600

    add dotfile for fish shell

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,3 @@
+# Important: create on entry for each container you want to list in the herdr sidebar.
+alias alenclaude='env HERDR_AGENT=claude docker exec -it alen-405-pgsql-webserver-1 bash -ic claude'
+alias alenopencode='HERDR_AGENT=opencode docker exec -it alen-405-pgsql-webserver-1 bash -ic opencode'
